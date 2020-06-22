20e2226fbdb0cb158424e63c4cef714897f099e2
MUDASSIRMunir <58942002+MUDASSIRMunir@users.noreply.github.com>
2020-06-22 18:24:28 +0500

    Update Addtransaction.js

--- src/components/Addtransaction.js
+++ src/components/Addtransaction.js
@@ -10,14 +10,19 @@ export const Addtransaction = () => {
 
     const onSubmit= e =>{
         e.preventDefault();
-
+        e.setState({ value: ""})
+    
         const newTransaction={ id: Math.floor(Math.random() * 10000000),
                                text,
                                amount: +amount   }
 
         addTransaction(newTransaction);
+    
                               
     }
+
+   
+
 
     return (
         <div>
@@ -25,18 +30,18 @@ export const Addtransaction = () => {
             <form onSubmit={onSubmit}> 
                 <div  className="form-control">
                     <label htmlFor="text">Text</label>
-                  <input type="text" value={text} onChange={(e) =>{setText(e.target.value)}}
-                   placeholder="Enter text..." required></input>
+                  <input type="text" id="mytext" onfocus="this.value=''" value={text} onChange={(e) =>{setText(e.target.value)}}
+                   placeholder="Enter text..."  required></input>
                 </div>
 
                 <div  className="form-control">
                     <label htmlFor="amount">Amount<br />
                     (negative-expense, positive-income)</label>
-                  <input type="number"  value={amount} onChange={(e) =>{setAmount(e.target.value)}}
+                  <input type="number"  id="amount" value={amount} onChange={(e) =>{setAmount(e.target.value)}}
                   placeholder="Enter amount..." required></input>
                 </div>
                 
-                <button className="btn">Add transaction</button>
+                <button  className="btn">Add transaction</button>
             </form>
         </div>
     )
